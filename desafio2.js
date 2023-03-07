cee2ab6883f2bcc47f4258845176de801bf7c068
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2023-03-06 22:31:45 -0300

    Agregado de metodo getProducts, donde toma todos los productos del info.json y los trae con readFile para mostrarlos en consola

--- desafio2.js
+++ desafio2.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { get } = require('http');
 const path = ('./info.json');
 class ProductManager {
     productArr = []
@@ -23,15 +24,30 @@ class ProductManager {
     const addData = async() =>{
     try {
         const data = await fs.promises.writeFile('./info.json',this.productArr); 
-        return console.log("Funciona");
+
     } catch (error) {
         console.log(error);
     }
         }
         addData();
+        console.log("Se agregó el producto");
 }
+    getProducts(){
+        const getData = async() =>{
+            try {
+                const data = await fs.promises.readFile('./info.json');
+                const objData = JSON.parse(data);
+                this.productArr.push(data);
+                console.log(objData);
+            } catch (error) {
+                
+            }
+        }
+       getData();
+    }
 }
 
 const newProduct = new ProductManager();
 newProduct.addProduct('Orange','An orange fruit',50,'Default','ABC123',50)
 newProduct.addProduct('Banana','A banana',75,"Default",'ABC124',15)
+newProduct.getProducts();
